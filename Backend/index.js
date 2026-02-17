65eb4bf5eeefdcc72c3cf8cdd92aeeab708cd668
Bladestar1370 <sumitdhuri61@gmail.com>
2026-02-17 14:10:03 +0530

    Backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const port = 4000;
+const port = process.env.PORT || 4000;
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -456,10 +457,7 @@ app.post('/orders', fetchUser, async (req, res) => {
     }
 });
 
-app.listen(port, (error) => {
-    if (!error) {
-        console.log('Server is running on port: ' + port);
-    } else {
-        console.error('Error: ' + error);
-    }
+
+app.listen(port, () => {
+    console.log('Server is running on port: ' + port);
 });
